6120ae9782ed371c44bb7ad2813f808b6998193d
AgnesEriksson <agnes.eriksson@mail.bcu.ac.uk>
2025-02-26 20:40:45 +0000

    Update Spawner.cs

--- FYP/Assets/Scripts/Spawner.cs
+++ FYP/Assets/Scripts/Spawner.cs
@@ -8,16 +8,35 @@ public class Spawner : MonoBehaviour
     [SerializeField] private Transform spawnPoint; //The SpawnPoint for the Obstacles
     [SerializeField] private float spawnRate = 2f; // How fast the Obstacles Spawn Serialized so easily changed
     [SerializeField] public float speedModifier = 1f; // To be use to speed up obstacles and spawning
+    private float timeSinceSpawn;
 
     void Start()
     {
         InvokeRepeating(nameof(SpawnObject), 0f, spawnRate * speedModifier); //Starts Spawn Cyle at Spawn Rate
     }
 
+    private void Update()
+    {
+        SpawnObject(); // Checks to see if spawn conditionas are met every frame
+    }
+
     private void SpawnObject()
     {
-        GameObject obstacle = Instantiate(Spawnables[Random.Range(0, Spawnables.Length)], spawnPoint.position, Quaternion.identity); // Instantiates from give array
+        timeSinceSpawn += Time.deltaTime; //enumaterates the spawn time every secnd
+        if (timeSinceSpawn >= spawnRate) // time since last spawn is greatere than the spawn rate triggers the spawn cycles and zeros it 
+        {
+            Spawn();
+            timeSinceSpawn = 0f;
+        }
+/*        GameObject obstacle = Instantiate(Spawnables[Random.Range(0, Spawnables.Length)], spawnPoint.position, Quaternion.identity); // Instantiates from give array
         Rigidbody2D oRB = obstacle.GetComponent<Rigidbody2D>();
-        oRB.velocity = Vector2.left * speedModifier;
+        oRB.velocity = Vector2.left * speedModifier;*/
+    }
+
+    private void Spawn()
+    {
+        GameObject obstacle = Instantiate(Spawnables[Random.Range(0, Spawnables.Length)], spawnPoint.position, Quaternion.identity); // Instantiates from give array
+        Rigidbody2D rb = obstacle.GetComponent<Rigidbody2D>();
+        rb.velocity = Vector2.left * speedModifier;
     }
 }
